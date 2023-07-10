30081142561c19f602d3ebbcc46186d137a5e840
elizabeth_roberts <elizabeth.roberts1927@gmail.com>
2023-07-10 17:18:19 -0300

    Added further styles to navbar.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,9 +5,12 @@ export default function Navbar() {
   return (
     <nav className="bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10">
         <div className="prose prose-xl mx-auto flex justify-between flex-col sm:flex-row">
+            <h1 className="text-3xl font-bold trext-white grid place-content-center mb-2 md:mb-0"> 
             <Link href="/"
                   className="text-white/90 no-underline hover:text-white">
-                    About</Link>  
+                    About
+            </Link>
+            </h1>  
         </div>
     </nav>
   );
