91af0b0de1a7d995740c575d907c36662ad2d7dd
elizabeth_roberts <elizabeth.roberts1927@gmail.com>
2023-07-10 16:59:36 -0300

    Added further style on header.

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>This is a <span className="font-bold">blog</span> blog</p>
+      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">This is a <span className="font-bold">blog</span> blog</p>
     </main>
   )
 }
