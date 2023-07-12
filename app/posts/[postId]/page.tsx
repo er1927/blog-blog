9e3edb6371bea52b211780f44d065380255c3dcb
elizabeth_roberts <elizabeth.roberts1927@gmail.com>
2023-07-12 14:19:39 -0300

    Created not found page.

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -1,6 +1,23 @@
 import { getSortedPostsData } from '@/lib/posts';
 import { notFound } from "next/navigation"
 
+export  function generateMetadata({params }: {params: {postId: string } }) {
+    const posts = getSortedPostsData() //deduped!
+    const { postId } = params  
+    
+    const post = posts.find(post => post.id === postId)
+
+    if (!post) {
+        return {
+            title: "Post not found"
+        }
+    }
+
+    return {
+        title: post.title,
+    }
+}
+
 export default async function Post({params }: {params: {postId: string } }) {
   const posts = getSortedPostsData() //deduped!
   const { postId } = params  
