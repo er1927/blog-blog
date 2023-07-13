070486a6b3772851678716c22013006cfc9c5407
elizabeth_roberts <elizabeth.roberts1927@gmail.com>
2023-07-13 08:44:28 -0300

    Adjusted spacing between main title and blog post ul.

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Posts from "./components/Posts"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-12">
+    <main className="flex min-h-screen flex-col items-center p-12">
       <p className="mt-6 mb-12 text-3xl text-center dark:text-white">This is a <span className="font-bold">blog</span> blog</p>
     <Posts />
     </main>
